b7519ac83d071969be91332b05a198ef44250509
Schascha <contact.schascha@gmail.com>
2018-11-01 23:09:33 +0100

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -37,9 +37,7 @@ module.exports = {
 	devServer: {
 		historyApiFallback: true,
 		overlay: true,
-		noInfo: true,
-		// contentBase: path.resolve(__dirname, 'docs'),
-		publicPath: '/dist/'
+		noInfo: true
 	},
 	watch: true,
 	plugins: [
