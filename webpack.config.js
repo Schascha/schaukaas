43969d29588bdc2746ab1d671617ee569ce396c2
Schascha <contact.schascha@gmail.com>
2018-11-01 22:54:19 +0100

    Add organic and pie sections

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const
+	path = require('path'),
+	webpack = require('webpack'),
+	ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/scss/main.scss',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist/',
+		filename: 'styles.css'
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+				use: [{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							ident: 'postcss',
+							plugins: [
+								require('autoprefixer'),
+								require('cssnano')
+							]
+						}
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			})
+		}]
+	},
+	devServer: {
+		historyApiFallback: true,
+		overlay: true,
+		noInfo: true,
+		// contentBase: path.resolve(__dirname, 'docs'),
+		publicPath: '/dist/'
+	},
+	watch: true,
+	plugins: [
+		new ExtractTextPlugin({
+			filename: 'styles.css',
+			allChunks: true
+		})
+	]
+}
